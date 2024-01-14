f41069a0580bdfbdd9c07a8af7c952880ea34a61
Ijustwantadadgumusername <spencerfrancisjr@gmail.com>
2024-01-13 21:34:48 -0600

    added a dadgum new line

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -33,6 +33,6 @@ class Square:
         """Prints the square with the # character"""
         for i in range(0, self.__size):
             [print("#", end="") for j in range(self.__size)]
-            print ("")
+            print("")
         if self.__size == 0:
             print("")
