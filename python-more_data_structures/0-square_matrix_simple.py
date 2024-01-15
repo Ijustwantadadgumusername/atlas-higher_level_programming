299b1a6152b2ba1ee6b464b19a51e8606be3c300
Ijustwantadadgumusername <spencerfrancisjr@gmail.com>
2024-01-15 10:43:26 -0600

    added file 0-bunchofwords

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def sqr(i):
+    res = i * i
+    return res
+
+
+def square_matrix_simple(matrix):
+    new_matrix = []
+    for i in range(len(matrix)):
+        new_matrix.append(list(map(sqr, matrix[i])))
+    return new_matrix
